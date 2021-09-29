f1e86f7c0ba4feb516d35526424cb2ac46f9126b
uzma <ubkhan3@gmail.com>
2021-09-29 14:12:45 +0000

    9/29 completed all functions. Started:9:50. Finished 10:12

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 //1
 function countToTen(){
-  
+  for(let i=1; i<=10; i++){
+      console.log(i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for(let i=1; i<=num; i++){
+      console.log(i);
+  }
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(num){
+  for(let i=1; i<=num; i++){
+      if(i % 2 === 0){
+      console.log(i);
+      }
+    }
 }
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for(let i=1; i<=num; i++){
+      if(i % 2 > 0){
+      console.log(i);
+      }
+  }
 }
 
 //5 
-function countEvens(){
-  
+function countEvens(arr){
+    let count = 0;
+  for(let i=1; i<=arr.length; i++){
+      if(arr[i] % 2 === 0){
+      count++;
+      }
+  }
+  return count;
 }
 
+countToTen();
+countFromOne(10);
+countEveryEven(10);
+countEveryOdd(10)
+console.log(countEvens([1,2,3,4,5,6,8]));
